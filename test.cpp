fd0c553e3853330465a27965ab863d28713e816f
1zs <yinzs@cqu.edu.cn>
2022-08-04 17:40:06 +0800

    add index.cpp

--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
   int a, b;
   cin >> a >> b;
-  
-  cout << a +b <<endl;
+
+  cout << a + b << endl;
 }
